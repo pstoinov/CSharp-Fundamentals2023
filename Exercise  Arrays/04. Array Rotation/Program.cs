d4817048a94d938124eddea8aaa292498df2572f
pstoinov <pstoinov@gmail.com>
2023-06-05 11:23:52 +0300

    Added some solved problems in Array Exercises

---
+++ Exercise  Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < rotations; i++)
+            //{
+            //    int firstElement = array[0];
+
+            //    for (int j = 0; j < array.Length - 1; j++)
+            //    {
+            //        array[j] = array[j + 1];
+            //    }
+
+            //    array[array.Length - 1] = firstElement;
+            //}
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstElement = array[0];
+                for (int j = 1; j < array.Length; j++)
+                {
+                    array[j - 1] = array[j];
+                }
+
+                array[^1] = firstElement;
+            }
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
---
+++ Exercise  Arrays/06. Equal Sums/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int index = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int leftSum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += arr[j];
+                }
+
+                int rightSum = 0;
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    rightSum += arr[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+                Console.WriteLine(index);
+            else
+                Console.WriteLine("no");
+
+        }
+    }
+}

1522a7ac6726b2793d5c0b3f4dcf2ba809c3f15c
pstoinov <pstoinov@gmail.com>
2023-06-06 13:15:14 +0300

    Added solved problems

---
+++ Exercise  Arrays/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == sum)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}

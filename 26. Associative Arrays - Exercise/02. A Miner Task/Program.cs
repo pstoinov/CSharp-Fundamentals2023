7a26739166a6e8402ff630389c70c87236aa5ab4
Plamen Stoynov <126239974+pstoinov@users.noreply.github.com>
2023-07-12 18:08:49 +0300

    Added stuff
    added some new stuffs

---
+++ 26. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, uint> resourceMap = new Dictionary<string, uint>();
+            string resource;
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                
+                uint quantity = uint.Parse(Console.ReadLine());
+
+                if (!resourceMap.ContainsKey((resource)))
+                {
+                    resourceMap.Add(resource, 0);
+                }
+                resourceMap[resource] += quantity;
+            }
+
+            foreach (var kvp in resourceMap)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}

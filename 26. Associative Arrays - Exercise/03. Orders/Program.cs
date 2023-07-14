fd4fe26ef91c851e8be1fd4c4b9642bda02a8801
Plamen Stoynov <126239974+pstoinov@users.noreply.github.com>
2023-07-14 17:39:30 +0300

    Added solved problems

---
+++ 26. Associative Arrays - Exercise/03. Orders/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Orders
+{
+    class Product
+    {
+        public string Name { get; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+
+        public Product(string name, decimal price, decimal quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public decimal TotalPrice => Price * Quantity;
+
+        public override string ToString()
+        {
+            return $"{Name} -> {TotalPrice:F2}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            var productList = new Dictionary<string, Product>();
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                var tokens = input.Split(' ');
+                var product = tokens[0];
+                var price = decimal.Parse(tokens[1]);
+                var quantity = decimal.Parse(tokens[2]);
+
+                if (!productList.ContainsKey(product))
+                {
+                    var productItem = new Product(product, price, quantity);
+                    productList.Add(product, productItem);
+                }
+                else
+                {
+                    var existingProduct = productList[product];
+                    existingProduct.Price = price;
+                    existingProduct.Quantity += quantity;
+                }
+            }
+
+            foreach (var productItem in productList.Values)
+            {
+                Console.WriteLine(productItem);
+            }
+        }
+    }
+}
---
+++ 26. Associative Arrays - Exercise/03.1 Orders/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Orders
+{
+    class Product
+    {
+        public Product(decimal price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            var productList = new Dictionary<string, Product>();
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                var tokens = input.Split(' ');
+                var product = tokens[0];
+                var price = decimal.Parse(tokens[1]);
+                var quantity = int.Parse(tokens[2]);
+
+                if (!productList.ContainsKey(product))
+                {
+                    var productItem = new Product(price, quantity);
+                    productList.Add(product, productItem);
+                }
+                else
+                {
+                    productList[product].Price = price;
+                    productList[product].Quantity += quantity;
+                }
+            }
+
+            foreach (var kvp in productList)
+            {
+                var productName = kvp.Key;
+                var totalPrice = kvp.Value.Price * kvp.Value.Quantity;
+                Console.WriteLine($"{productName} -> {totalPrice:F2}");
+            }
+        }
+    }
+}
